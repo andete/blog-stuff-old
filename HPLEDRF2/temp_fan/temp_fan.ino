29c8a173778649424a9630d3c49e6e00b995dba6
Joost Yervante Damad <joost@damad.be>
2013-07-04 16:12:05 +0200

    basic functionality for temp_fan sketch

--- HPLEDRF2/temp_fan/temp_fan.ino
+++ HPLEDRF2/temp_fan/temp_fan.ino
@@ -5,10 +5,21 @@
 #include <JeeLib.h>
 #include <Wire.h>
 
+#define RF_GROUP 100
+#define RF_ID 3
+
 // PD3
 #define FAN_PULSE 3
 // PD5
 #define FAN_PWM 5
+
+// ADC7
+#define TEMP_PIN A7
+
+// ADC is 5.0/1024 volt per adc step
+// ad8495 is 5mV/C
+static const float reference_voltage = 5.0;
+static const float adc_factor = (reference_voltage/1024)/(5E-3);
 
 static inline bool rf_available() {
   return (rf12_recvDone() && rf12_crc == 0 && rf12_len >= 1);
@@ -23,6 +34,26 @@ static void pulse_interrupt() {
     ++pulse_count;
   }
 
+static int8_t temperature = 0;
+static uint8_t pwm_setting = 128;
+
+static inline void send_data() {
+  static uint8_t data[4];
+  data[0] = temperature;
+  data[1] = pulse_count / 256;
+  data[2] = pulse_count % 256;
+  data[3] = pwm_setting;
+  rf12_sendStart(RF12_ACK_REPLY, data, 4);
+  rf12_sendWait(1);
+  Serial.println("[send_data]");
+}
+
+static inline void set_pwm() {
+ pwm_setting = rf12_data[1];
+ analogWrite(FAN_PWM, pwm_setting);
+ rf_ack();
+  Serial.println("[set_pwm]");
+}
 
 void setup() {
   // Serial
@@ -30,27 +61,37 @@ void setup() {
   Serial.println("Hello.");
 
   // RF
-  rf12_initialize(1, RF12_868MHZ, 100);
+  rf12_initialize(RF_ID, RF12_868MHZ, RF_GROUP);
 
   // PWM
   pinMode(FAN_PWM, OUTPUT);
-  analogWrite(FAN_PWM, 128);
+  analogWrite(FAN_PWM, pwm_setting);
 
   // fan pulse
   attachInterrupt(1, pulse_interrupt, FALLING);
+
+  // set ADC reference to internal 1.1V reference
+  // internal ref is +- 0.1V
+  // 5V Vreg is +-0.025V, so using the internal reference is far worse
+  // analogReference(INTERNAL);
 }
+
 
 void loop() {
   static uint8_t lc = 0;
   if (rf_available()) {
     const uint8_t cmd = rf12_data[0];
     switch (cmd) {
-      case 2: analogWrite(FAN_PWM, rf12_data[1]); rf_ack(); break;
+      case 1: send_data(); break;
+      case 2: set_pwm(); break;
     } 
   }
   delay(100);
   ++lc;
   if (lc % 16 == 0) {
      // take temperature, check fan pulse
+     const float raw_temp = analogRead(TEMP_PIN); // steps
+     temperature = raw_temp * adc_factor;
+     Serial.print(temperature); Serial.print(" "); Serial.println(pulse_count);
   }
 }
